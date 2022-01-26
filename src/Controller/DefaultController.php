a7d7b296f02c6408684eeff3a38e665d54ebef67
MaevaWolff <maeva.wolff@hetic.net>
2022-01-26 21:39:41 +0100

    feat(controller): add finAll method in index

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,8 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render('pages/index.html.twig');
+        return $this->render('pages/index.html.twig', ['posts' => $postRepository->findAll()]);
     }
-}
+} 
