45d8d629b1e10dd5a264abcad7869be9a3ed81d1
MaevaWolff <maeva.wolff@hetic.net>
2022-01-26 19:44:48 +0100

    feat: create all Factory files to add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
+use App\Factory\TagFactory;
+use App\Factory\PostFactory;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +13,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::createMany(10);
+
+        // NOTE: To create some tags
+        TagFactory::createOne(['name' => 'jeux']);
+        TagFactory::createOne(['name' => 'véhicules']);
+        TagFactory::createOne(['name' => 'loisirs']);
+        TagFactory::createOne(['name' => 'divers']);
+
+        PostFactory::createMany(10);
+
 
         $manager->flush();
     }
